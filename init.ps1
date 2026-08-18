43a636ec957f92d21cfd33f11bbb889f146c09ac
457 <1042598292@qq.com>
2026-08-18 17:08:28 +0800

    调整项目结构，减少不必要的顶层文件

--- init.ps1
+++ init.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'stop'
 
 # 初始化PROFILE
-(Get-Content $PSScriptRoot\profile.ps1 | ForEach-Object { $_.Replace('ROOT', $PSScriptRoot) } ) > $PROFILE
+(Get-Content $PSScriptRoot\env\profile.ps1 | ForEach-Object { $_.Replace('ROOT', $PSScriptRoot) } ) > $PROFILE
 
 # 输出config.json
 $config = Import-PowerShellDataFile -Path $PSScriptRoot\env\config.psd1
@@ -11,11 +11,14 @@ $config | ConvertTo-Json -Depth 20 | Out-File -FilePath $PSScriptRoot\env\config
 $clear_config = @{}
 foreach ($kv in $config.getenumerator()) { $clear_config[$kv.key] = $kv.value.gettype().name }
 $clear_config.GetEnumerator() | ForEach-Object { $out = "" } { $out += "`"$($_.key)`"=`"$($_.value)`"`n" }
-"
-# check path of prs with like ~\swq\prs
-# shell version is pwsh 7
-# python version is 3.11
-# copy the context to `"env\config.psd1`" and input value
+"<# following these step:
+1.check path of prs with like ~\swq\prs
+2.check pwsh version is 7.
+3.check python version is 3.11
+4.copy the context to `"env\config.psd1`" and input value
+5.execute the file as init.ps1
+6.make venv of pr and install with pip.txt
+#>
 
 @{`n$out}
 " `
