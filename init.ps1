8a55d26d8b47957d55ed1be9b8aaf70f14009bb7
457 <1042598292@qq.com>
2026-08-18 13:00:50 +0800

    优化环境变量结构，去除ROOT；优化项目基础信息

--- init.ps1
+++ init.ps1
@@ -1,22 +1,25 @@
 $ErrorActionPreference = 'stop'
 
-# 初始化
-$config = Import-PowerShellDataFile -Path $PSScriptRoot\env\config.psd1
-(Get-Content $PSScriptRoot\profile.ps1 | ForEach-Object { $_.Replace('ROOT', $config["ROOT"]) } ) > $PROFILE
-$config | ConvertTo-Json -Depth 20 | Out-File -FilePath $PSScriptRoot\env\config.json -Encoding utf8
+# 初始化PROFILE
+(Get-Content $PSScriptRoot\profile.ps1 | ForEach-Object { $_.Replace('ROOT', $PSScriptRoot) } ) > $PROFILE
 
+# 输出config.json
+$config = Import-PowerShellDataFile -Path $PSScriptRoot\env\config.psd1
+$config | ConvertTo-Json -Depth 20 | Out-File -FilePath $PSScriptRoot\env\config.json -Encoding utf8
 
 # 脱敏输出init.txt
 $clear_config = @{}
-foreach ($kv in $config.getenumerator()) {
-    $clear_config[$kv.key] = 
-    if ($kv.key -eq 'ROOT') { $kv.value }
-    else { $kv.value.gettype().name } 
-}
+foreach ($kv in $config.getenumerator()) { $clear_config[$kv.key] = $kv.value.gettype().name }
 $clear_config.GetEnumerator() | ForEach-Object { $out = "" } { $out += "`"$($_.key)`"=`"$($_.value)`"`n" }
-"# copy to `"env\config.psd1`" and input value`n# keep structure of ROOT with like ~\swq\prs`n`n@{`n$out}`n" > $PSScriptRoot\init.txt
+"
+# check path of prs with like ~\swq\prs
+# shell version is pwsh 7
+# python version is 3.11
+# copy the context to `"env\config.psd1`" and input value
 
+@{`n$out}
+" `
+    > $PSScriptRoot\init.txt
 
 # 退出状态
 Write-Host '>_'
-
