e32c83489047897461666e3246546e5d1c817379
457 <1042598292@qq.com>
2026-08-30 14:46:09 +0800

    删除终端装饰

--- env/profile.ps1
+++ env/profile.ps1
@@ -1,7 +1,3 @@
-# 终端装饰
-Invoke-Expression (starship init powershell)
-$PSVersionTable | Out-String -Stream | Select-Object -First 5
-
 # 个人配置
 Import-Module ROOT\wq.psm1 -DisableNameChecking -Force
 if ($env:path -notmatch '\\prs\\') { $env:path += ";ROOT\bin" }
