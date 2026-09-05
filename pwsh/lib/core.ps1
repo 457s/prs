d843d75f9da36aa9c9da8e58829779227653908d
457 <1042598292@qq.com>
2026-09-05 23:29:57 +0800

    入口基本完成，需要复查一下

--- pwsh/lib/core.ps1
+++ pwsh/lib/core.ps1
@@ -2,108 +2,98 @@
     param(
         [string]$name
     )
-    Get-Help $name -Full
+    if ($name -eq $null -or $name -eq "") {
+        Get-Content $HOME\core\prs\pwsh\lib\core.ps1 |
+        ForEach-Object { [regex]::Match($_, "^function(.+){$").Groups[1].Value } |Where-Object{$_}|Sort-Object
+    }
+    else {
+        (Get-Command $name).Definition
+    }
 }
 
-
-function wq_prs {
-    Set-Location ~\wq\prs
+function prs {
+    Set-Location $HOME\core\prs
     try { & .\.venv\scripts\activate.ps1 }catch {}
 }
 
-function wq_mine {
-    Set-Location ~\wq\prs\mine
-    try { & .\.venv\scripts\activate.ps1 }catch {}
-}
-Set-Alias -Name wq_mi -Value wq_mine
-
-function wq_ai {
-    Set-Location ~\wq\prs\ai
+function mi {
+    Set-Location $HOME\core\prs\mine
     try { & .\.venv\scripts\activate.ps1 }catch {}
 }
 
 
-
-function wq_prsspace {
-    wq_prs; Get-ChildItem -File | Where-Object { $_.Extension -eq '.code-workspace' } | ForEach-Object { explorer.exe $_.FullName }
+function ai {
+    Set-Location $HOME\core\prs\ai
+    try { & .\.venv\scripts\activate.ps1 }catch {}
 }
-Set-Alias -Name wq_prss -Value wq_prsspace
 
-function wq_minespace {
-    wq_mine; Get-ChildItem -File | Where-Object { $_.Extension -eq '.code-workspace' } | ForEach-Object { explorer.exe $_.FullName }
+
+
+function prss {
+    prs; Get-ChildItem -File | Where-Object { $_.Extension -eq '.code-workspace' } | ForEach-Object { explorer.exe $_.FullName }
 }
-Set-Alias -Name wq_mis -Value wq_minespace
 
-function wq_aispace {
-    wq_ai; Get-ChildItem -File | Where-Object { $_.Extension -eq '.code-workspace' } | ForEach-Object { explorer.exe $_.FullName }
+
+function mis {
+    mi; Get-ChildItem -File | Where-Object { $_.Extension -eq '.code-workspace' } | ForEach-Object { explorer.exe $_.FullName }
 }
-Set-Alias -Name wq_ais -Value wq_aispace
 
-function wq_main {
+
+function ais {
+    ai; Get-ChildItem -File | Where-Object { $_.Extension -eq '.code-workspace' } | ForEach-Object { explorer.exe $_.FullName }
+}
+
+
+function pym {
     python .\main.py
 }
-Set-Alias -Name wq_m -Value wq_main
 
 
-function wq_minemain {
-    wq_mine; wq_main
+
+function mim {
+    mi; pym
 }
-Set-Alias -Name wq_mim -Value wq_minemain
 
 
-
-function wq_aimain {
-    wq_ai; wq_main
+function aim {
+    ai; pym
 }
-Set-Alias -Name wq_aim -Value wq_aimain
 
 
-function wq_checkvenv {
+function cv {
     $check = Get-ChildItem env: | Where-Object { $_.name -like '*venv*' -or $_.value -like '*venv*' }
     if ($check) { Write-Host "(venv status)" -ForegroundColor Yellow; $check }else { Write-Host "(venv status is null)" -ForegroundColor Yellow }
 }
-Set-Alias -Name wq_cv -Value wq_checkvenv
 
 
-function wq_restart {
-    Start-Process pwsh -NoNewWindow -UseNewEnvironment -ArgumentList '-noexit', '-command', "write-host;wq_checkvenv"
+function rs {
+    Start-Process pwsh -NoNewWindow -UseNewEnvironment -ArgumentList '-noexit', '-command', "write-host;pws cv"
     [System.Environment]::Exit(0)
 }
-Set-Alias -Name wq_rs -Value wq_restart
 
 
 
-function wq_qwen {
+function qw {
     ollama list | ForEach-Object { $_.Split(' ')[0] } | Select-Object -Skip 1 | ForEach-Object { $module = @{}; $c = 0 } { $c++; $module["id_$c"] = $_ } { $module } 
     $id = Read-Host "input module id"
     Write-Host "`n>$($module["id_$id"])<"
     ollama run $module["id_$id"]
 }
-Set-Alias -Name wq_qw -Value wq_qwen
 
 
-function wq_aiwork {
-    <#
-    .PARAMETER n
-        文件夹名称
-    .PARAMETER d
-        任务描述
-    .PARAMETER file_paths
-        打包参数
-    #>
+function aiw {
     param(
         [string]$n,
         [string]$d,
         [Parameter(ValueFromRemainingArguments = $true)]    
         [string[]]$file_paths
     )
-    $ROOT = Split-Path(Split-Path $PSScriptRoot -Parent) -Parent
     if ($n -ne 't') {
         $dest_folder_name = $n
     }
     else { $dest_folder_name = Get-Date -Format "yy年MM月dd日HH时mm分ss秒" }
     if ($file_paths) {
-        $dest_folder_path_old = Join-Path $Root "ai\works\$dest_folder_name"
+        $dest_folder_path_old = "$HOME\core\prs\ai\works\$dest_folder_name"
         if (Test-Path $dest_folder_path_old) { $c = 0; $e = '!'; do { $c++; $dest_folder_path = $dest_folder_path_old + ($e * $c) }while (Test-Path $dest_folder_path) }
         else { $dest_folder_path = $dest_folder_path_old }
         New-Item -ItemType Directory -Path $dest_folder_path
@@ -118,22 +108,20 @@ function wq_aiwork {
     }
     else { Write-Host 'please input the paths of files you want to copy' }
 }
-Set-Alias -Name wq_aiw -Value wq_aiwork
 
 
 
-function wq_init {
-    $ROOT = Split-Path(Split-Path $PSScriptRoot -Parent) -Parent
-    & $Root\boot\pwsh\init.ps1
-    wq_restart
-}
-Set-Alias -Name wq_it -Value wq_init
-
-function wq_gt {
-    wq_prs; git status
+function it {
+    & $HOME\core\prs\pwsh\init.ps1
+    rs
 }
 
-function wq_explorer {
+function gt {
+    prs; git status
+}
+
+
+function er {
     param(
         [string]$search
     )
@@ -151,4 +139,3 @@ function wq_explorer {
     }
     else { Write-Host 'not find with startapps' }
 }
-Set-Alias -Name wq_r -Value wq_explorer
