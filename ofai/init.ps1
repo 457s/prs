42c9c2bb2453ecc0c153178480262dfd40284d44
457 <1042598292@qq.com>
2026-09-06 20:02:00 +0800

    补充初始化结果

--- ofai/init.ps1
+++ ofai/init.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'stop'
 
 Set-Location $PSScriptRoot
-if (Test-Path .\.venv) {}else {
+if (Test-Path .\.venv) { Write-Host '.venv is exists' -ForegroundColor Green }else {
     python -m venv .venv;
     $ErrorActionPreference = 'continue'
     & .\.venv\scripts\pip.exe install -r requirements.txt
