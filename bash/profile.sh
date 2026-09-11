756cb3d4f04b82b816631773f8bb9b18c2b381da
mybook <1042598292@qq.com>
2026-09-11 22:48:00 +0800

    .

--- bash/profile.sh
+++ bash/profile.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# 提示符
+PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+
+# PATH
 if [[ "$PATH" != *"$HOME/core/prs/bin"* ]]; then
     PATH="$HOME/core/prs/bin:$PATH"
 fi
--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -1,1 +1,10 @@
+# 提示符
+function prompt {
+    Write-Host "PS " -NoNewline -ForegroundColor Green
+    Write-Host $executionContext.SessionState.Path.CurrentLocation -NoNewline -ForegroundColor Blue
+    Write-Host "> " -NoNewline
+    " "
+}
+
+# PATH
 if ($env:path -notmatch '\\core\\prs\\bin') { $env:path += ";$HOME\core\prs\bin" }
