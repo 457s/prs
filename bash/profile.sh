3a0020272cefb2123086a55a9a7431684056e15d
mybook <1042598292@qq.com>
2026-09-12 11:19:44 +0800

    .

--- bash/profile.sh
+++ bash/profile.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #! /bin/bash
 
 # 提示符
@@ -8,3 +9,14 @@ if [[ "$PATH" != *"$HOME/core/prs/bin"* ]]; then
     PATH="$HOME/core/prs/bin:$PATH"
 fi
 
+=======
+#! /bin/bash
+
+if [[ "$PATH" != *"$HOME/core/prs/bin"* ]]; then
+    PATH="$HOME/core/prs/bin:$PATH"
+fi
+
+if [[ "$PATH" != *"$HOME/bin"* ]]; then
+    PATH="$HOME/bin:$PATH"
+fi
+>>>>>>> refs/remotes/prshub/main
