9f25fa99a58c576a437ec342ae688af53bb5c2f2
mybook-ubuntu <1042598292@qq.com>
2026-09-12 11:46:31 +0800

    .

--- bash/profile.sh
+++ bash/profile.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #! /bin/bash
 
 # 提示符
@@ -8,3 +9,19 @@ if [[ "$PATH" != *"$HOME/core/prs/bin"* ]]; then
     PATH="$HOME/core/prs/bin:$PATH"
 fi
 
+=======
+#! /bin/bash
+
+# 提示符
+PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+
+# PATH
+if [[ "$PATH" != *"$HOME/core/prs/bin"* ]]; then
+    PATH="$HOME/core/prs/bin:$PATH"
+fi
+
+if [[ "$PATH" != *"$HOME/bin"* ]]; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+>>>>>>> f2ac163 (.)
--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -1,10 +1,15 @@
 # 提示符
 function prompt {
     Write-Host "PS " -NoNewline -ForegroundColor Green
+<<<<<<< HEAD
     Write-Host $executionContext.SessionState.Path.CurrentLocation -NoNewline -ForegroundColor Cyan
+=======
+    Write-Host $executionContext.SessionState.Path.CurrentLocation -NoNewline -ForegroundColor Blue
+>>>>>>> f2ac163 (.)
     Write-Host "> " -NoNewline
     " "
 }
 
 # PATH
 if ($env:path -notmatch '\\core\\prs\\bin') { $env:path += ";$HOME\core\prs\bin" }
+
